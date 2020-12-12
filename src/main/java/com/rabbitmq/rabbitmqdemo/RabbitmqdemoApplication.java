8754d091707e3919dd89cb154aef8aade3ac7b07
nycrera <nycrera@gmail.com>
2020-12-12 18:55:00 +0300

    Simple message added to rabbitmq.

--- src/main/java/com/rabbitmq/rabbitmqdemo/RabbitmqdemoApplication.java
+++ src/main/java/com/rabbitmq/rabbitmqdemo/RabbitmqdemoApplication.java
@@ -1,13 +1,28 @@
 package com.rabbitmq.rabbitmqdemo;
 
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RabbitmqdemoApplication {
+public class RabbitmqdemoApplication implements CommandLineRunner {
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
 
     public static void main(String[] args) {
         SpringApplication.run(RabbitmqdemoApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+
+        SimpleMessage message = new SimpleMessage();
+        message.setName("FirstMessage");
+        message.setDescription("SimpleDescription");
+
+        rabbitTemplate.convertAndSend("TestExchange", "testRouting", message);
+    }
 }
